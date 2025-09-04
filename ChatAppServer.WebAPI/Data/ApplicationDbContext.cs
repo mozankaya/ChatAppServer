a6421859c2110697b71554287c779b8db1e609c7
mozankaya <mehmetozankaya@gmail.com>
2025-09-04 16:45:26 +0300

    add user-chat entites and DbContext

---
+++ ChatAppServer.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatAppServer.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatAppServer.WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users {  get; set; }
+        public DbSet<Chat> Chats { get; set; }
+
+    }
+}
